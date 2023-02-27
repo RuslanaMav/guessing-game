f714fab1b7e287c39e9e8f5efe0750586eb2af2a
RuslanaMav <116889933+RuslanaMav@users.noreply.github.com>
2023-02-27 19:02:03 +0500

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guess = Math.round((this.max + this.min) / 2);
+        return this.guess;
     }
 
     lower() {
-
+        this.max = this.guess;
     }
 
     greater() {
-
+        this.min = this.guess;
     }
 }
 
